0fc85ec3e102802dbfc991e6e1c5174cb899a1c4
Cherepanova Ekaterina <ekatbobo@gmail.com>
2022-09-26 18:54:34 +0300

    Fix task_3

--- task_3/3.3.py
+++ task_3/3.3.py
@@ -1,4 +1,8 @@
+def clock(n):
+    h = n % (60 * 24) // 60
+    n = n % 60
+    print(h, ':', n)
+
+
 n = int(input('Введите количество минут:'))
-h = n % (60 * 24) // 60
-n = n % 60
-print(h, ':', n)
+clock(n)
