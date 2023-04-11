7cf0d7276240e5da16205a42825d986cbbe84570
Cherepanova-ES <113259896+Cherepanova-ES@users.noreply.github.com>
2023-04-11 08:36:59 +0300

    Delete task_2 directory

--- task_2/2.1.py
+++
@@ -1,8 +0,0 @@
-import math
-
-x = float(input('Введите переменную x:'))
-y = float(input('Введите переменную y:'))
-z = float(input('Введите переменную z:'))
-s = (math.pow(y + math.pow(x - 1, 1 / 3), 1 / 4)) / \
-    ((math.fabs(x - y)) * (math.sin(z) ** 2 + math.tan(z)))
-print('s=', s)
