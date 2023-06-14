a16444213c7c6e84f62688ab985c956af5842c41
Cherepanova-ES <ekatbobo@gmail.com>
2023-06-14 11:31:50 +0300

    task 49

---
+++ src/main/java/task49/Main.java
@@ -0,0 +1,25 @@
+package task49;
+
+public class Main {
+    public static int fibon(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        System.out.println(fibon(0) + "\n" + fibon(1) +
+                "\n" + fibon(2) + "\n" + fibon(13));
+    }
+}
